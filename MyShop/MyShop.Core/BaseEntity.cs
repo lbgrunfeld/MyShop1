1eda5005cdcdbe48d6ced889616dc0e56deaa8da
laszlo grunfeld <lbgrunfeld@gmail.com>
2019-08-08 17:43:48 -0400

    Implement SQL Repository

--- MyShop/MyShop.Core/BaseEntity.cs
+++ MyShop/MyShop.Core/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace MyShop.Core
 {
     public abstract class BaseEntity
     {
-        public string Id;
+        [Key]
+        public string Id { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
 
         public BaseEntity()
